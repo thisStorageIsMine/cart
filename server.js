5401be9334634efb0ebd87b7cebaafe69e3a650f
thisStorageIsMine <bezrukovvaler@yandex.ru>
2024-03-31 16:32:42 +0300

    refactor: replace dbSettings.js with .env file

--- server.js
+++ server.js
@@ -3,18 +3,16 @@ import fastify from "fastify";
 import mysql from 'mysql2/promise';
 
 
-const PORT = 2861; //На этом порту будет работать сервер!
-
 // Создаю сервер через библеотеку
 const server = fastify();
 
 // Настраиваю бд
 const pool = mysql.createPool({
     connectionLimit: 100,
-    host: dbSettings.HOST,
-    user: dbSettings.USER,
-    password: dbSettings.PASSWORD,
-    database: dbSettings.DATABASE
+    host: process.env.HOST,
+    user: process.env.DBUSER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
 });
 
 // Роуты
@@ -31,12 +29,12 @@ server.route({
         querystring: {
             type: 'object',
             properties: {
-                name: { type: 'string' },               
+                name: { type: 'string' },
                 price: { type: 'number' },
                 amount: { type: 'number' }
             },
             required: ['name', 'price', 'amount'],
-          },                
+        },
 
     },
     handler: async (req, reply) => {
@@ -48,27 +46,27 @@ server.route({
             });
 
             reply
-                 .code(200)
-                 .header('Content-type', 'application/json; charset=utf-8')
-                 .send({message: `Товар ${req.query.name} успешно добавлен`})
+                .code(200)
+                .header('Content-type', 'application/json; charset=utf-8')
+                .send({ message: `Товар ${req.query.name} успешно добавлен` })
         } catch (error) {
-            
+
             reply
-                 .code(500)
-                 .header('Content-type', 'application/json; charset=utf-8')
-                 .send({message: error})
+                .code(500)
+                .header('Content-type', 'application/json; charset=utf-8')
+                .send({ message: error })
 
 
         }
-        
+
     }
 })
 
 
 
 try {
-    server.listen({port: PORT});
-    console.log(`Сервер работает по адресу http://localhost:${PORT}`);
+    server.listen({ port: process.env.PORT });
+    console.log(`Сервер работает по адресу http://localhost:${process.env.PORT}`);
 } catch (err) {
     console.log('Сервер не запустился, вот ошибка: ', err);
 }
@@ -76,7 +74,7 @@ try {
 
 
 
-// Функции
+// Функции для работы с БД
 async function getUsers() {
     return await pool.query('SELECT * FROM users');
 }
